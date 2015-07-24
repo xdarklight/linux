d602837a5a726052c77f5f6ee9762a0c0e12a1d5
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2015-07-24 14:12:53 +0200

    clk: lantiq: lantiq: Add initial common clock support for lantiq SoCs
    This ports the arch-specific clock implementations to the common clk
    framework.

---
+++ drivers/clk/lantiq/clk-grx390.c
@@ -0,0 +1,231 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * Copyright (C) 2015 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ */
+
+#include <dt-bindings/clock/lantiq-xway-pmu.h>
+#include <linux/clk.h>
+#include <linux/clkdev.h>
+#include <linux/err.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/types.h>
+
+#include "clk-xway.h"
+
+#define PMU_CLKGSR1		0x20
+#define PMU_CLKGCR1_A		0x24
+#define PMU_CLKGCR1_B		0x28
+#define PMU_CLKGSR2		0x30
+#define PMU_CLKGCR2_A		0x34
+#define PMU_CLKGCR2_B		0x38
+#define PMU_ANALOG_SR		0x40
+#define PMU_ANALOGCR_A		0x44
+#define PMU_ANALOGCR_B		0x48
+
+#define CGU_IF_CLK_XRX300	0x24
+
+static unsigned long ltq_grx390_sys_recalc_rate(struct clk_hw *hw,
+						unsigned long parent_rate)
+{
+	struct ltq_cgu_clk *cgu_clk = to_ltq_cgu_clk(hw);
+	unsigned int sys_sel;
+	unsigned long rate;
+
+	sys_sel = (ltq_cgu_clk_read(cgu_clk, CGU_SYS_XRX) >> 9) & 0x3;
+
+	switch (sys_sel){
+	case 0:
+		rate = CLOCK_600M;
+		break;
+	case 1:
+		rate = CLOCK_666M;
+		break;
+	case 2:
+		rate = CLOCK_720M;
+		break;
+	default:
+		pr_warn("%s: Unknown sys sel: %u\n", __func__, sys_sel);
+		rate = CLOCK_600M;
+		break;
+	}
+
+	return rate;
+}
+
+static unsigned long ltq_grx390_cpu_recalc_rate(struct clk_hw *hw,
+						unsigned long parent_rate)
+{
+	struct ltq_cgu_clk *cgu_clk = to_ltq_cgu_clk(hw);
+	unsigned int cpu_div;
+	unsigned long rate;
+
+	cpu_div = ltq_cgu_clk_read(cgu_clk, CGU_SYS_XRX) >> 4 & 0x7;
+
+	switch (cpu_div) {
+	case 0:
+		rate = parent_rate;
+		break;
+	case 1:
+		rate = parent_rate / 2;
+		break;
+	case 2:
+		rate = parent_rate / 4;
+		break;
+	default:
+		pr_warn("%s: Unknown cpu div: %u\n", __func__, cpu_div);
+		rate = parent_rate;
+		break;
+	}
+
+	return rate;
+}
+
+unsigned long ltq_grx390_fpi_recalc_rate(struct clk_hw *hw,
+					 unsigned long parent_rate)
+{
+	struct ltq_cgu_clk *cgu_clk = to_ltq_cgu_clk(hw);
+	unsigned int fpi_div;
+	unsigned long rate;
+
+	fpi_div = ltq_cgu_clk_read(cgu_clk, CGU_SYS_XRX) & 0x7;
+
+	switch (fpi_div) {
+	case 0:
+		rate = parent_rate;
+		break;
+	case 1:
+		rate = parent_rate / 2;
+		break;
+	case 2:
+		rate = parent_rate / 4;
+		break;
+	default:
+		pr_warn("%s: Unknown fpi div: %u\n", __func__, fpi_div);
+		rate = parent_rate;
+		break;
+	}
+
+	return rate;
+}
+
+unsigned long ltq_grx390_pp32_recalc_rate(struct clk_hw *hw,
+					  unsigned long parent_rate)
+{
+	struct ltq_cgu_clk *cgu_clk = to_ltq_cgu_clk(hw);
+	unsigned int ppe_sel;
+	unsigned long rate;
+
+	ppe_sel = (ltq_cgu_clk_read(cgu_clk, CGU_SYS) >> 16) & 0x7;
+
+	switch (ppe_sel) {
+	case 1:
+		rate = CLOCK_250M;
+		break;
+	case 2:
+		rate = CLOCK_432M;
+		break;
+	case 4:
+		rate = CLOCK_400M;
+		break;
+	default:
+		pr_warn("%s: Unknown ppe sel: %u\n", __func__, ppe_sel);
+		rate = CLOCK_250M;
+		break;
+	}
+
+	return rate;
+}
+
+const struct clk_ops grx390_sys_clk_ops = {
+	.recalc_rate = ltq_grx390_sys_recalc_rate,
+};
+
+const struct clk_ops grx390_cpu_clk_ops = {
+	.recalc_rate = ltq_grx390_cpu_recalc_rate,
+};
+
+const struct clk_ops grx390_fpi_clk_ops = {
+	.recalc_rate = ltq_grx390_fpi_recalc_rate,
+};
+
+const struct clk_ops grx390_pp32_clk_ops = {
+	.recalc_rate = ltq_grx390_pp32_recalc_rate,
+};
+
+static void __init grx390_cgu_clocks_init_dt(struct device_node *np)
+{
+	struct ltq_cgu_clocks cgu_clocks = {};
+	struct clk *sys_clk;
+
+	ltq_xrx_cgu_select_gphy_clk_input(np);
+
+	sys_clk = ltq_cgu_register_clk("sys", NULL, &grx390_sys_clk_ops);
+	if (clk_register_clkdev(sys_clk, "sys", NULL))
+		pr_err("%s: Failed to register the sys clkdev\n", __func__);
+
+	cgu_clocks.cpu_clk = ltq_cgu_register_clk("cpu", "sys",
+						  &grx390_cpu_clk_ops);
+	cgu_clocks.fpi_clk = ltq_cgu_register_clk("fpi", "sys",
+						  &grx390_fpi_clk_ops);
+	cgu_clocks.io_clk = cgu_clocks.fpi_clk; /* FPI clock is used for IO */
+	cgu_clocks.pp32_clk = ltq_cgu_register_clk("pp32", NULL,
+						   &grx390_pp32_clk_ops);
+
+	ltq_cgu_clk_of_add_provider(np, &cgu_clocks);
+}
+
+CLK_OF_DECLARE(cgu_grx390, "lantiq,grx390-cgu", grx390_cgu_clocks_init_dt);
+
+static struct ltq_xway_pmu_gate grx390_pmu_gates[] __initdata = {
+	PMU_GATE(DFEV0, "dfev0", 2, NULL, 0, NULL, NULL),
+	PMU_GATE(DFEV1, "dfev1", 3, NULL, 0, NULL, NULL),
+	PMU_GATE(DMA, "dma", 5, NULL, 0, "1e104100.dma", NULL),
+	PMU_GATE(USB0_CTRL, "usb0_ctrl", 6, NULL, 0, NULL, NULL),
+	PMU_GATE(USIF, "usif", 7, NULL, 0, NULL, NULL),
+	PMU_GATE(SPI, "spi", 8, NULL, 0, "1e100800.spi", NULL),
+	PMU_GATE(DSL_DFE, "dsl_dfe", 9, NULL, 0, NULL, NULL),
+	PMU_GATE(EBU, "ebu", 10, NULL, CLK_IGNORE_UNUSED, "1e105300.ebu", NULL),
+	PMU_GATE(STP, "ledc", 11, NULL, 0, "1e100bb0.stp", NULL),
+	PMU_GATE(GPTC, "gptu", 12, NULL, 0, "1e100a00.gptu", NULL),
+	PMU_GATE(UART1, "serial1", 17, NULL, CLK_IGNORE_UNUSED, "1e100c00.serial", NULL),
+	PMU_GATE(DEU, "deu", 20, NULL, 0, NULL, NULL),
+	PMU_GATE(PPE_TC, "ppe_tc", 21, NULL, 0, NULL, NULL),
+	PMU_GATE(PPE_EMA, "ppe_ema", 22, NULL, 0, NULL, NULL),
+	PMU_GATE(PPE_DPLUS, "ppe_dplus", 23, NULL, 0, "1e108000.eth", "ppe_dplus"),
+	PMU_GATE(TDM, "tdm", 25, NULL, 0, NULL, NULL),
+	PMU_GATE(USB1_CTRL, "usb1_ctrl", 27, NULL, 0, NULL, NULL),
+	PMU_GATE(SWITCH, "switch", 28, NULL, 0, "1e108000.eth", "switch"),
+	PMU_GATE(PCIE0_CTRL, "pcie0_ctrl", 33, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE1_CTRL, "pcie1_ctrl", 34, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE0_PDI, "pcie0_pdi", 36, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE0_MSI, "pcie0_msi", 37, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE1_PDI, "pcie1_pdi", 52, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE1_MSI, "pcie1_phy", 53, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE2_CTRL, "pcie2_ctrl", 57, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE2_PDI, "pcie2_pdi", 58, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE2_MSI, "pcie2_msi", 59, NULL, 0, NULL, NULL),
+	PMU_GATE(USB0_PHY, "usb0_phy", 64, NULL, 0, NULL, NULL),
+	PMU_GATE(USB1_PHY, "usb1_phy", 65, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE0_PHY, "pcie0_phy", 72, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE1_PHY, "pcie1_phy", 73, NULL, 0, NULL, NULL),
+	PMU_GATE(PCIE2_PHY, "pcie2_phy", 74, NULL, 0, NULL, NULL),
+	PMU_GATE(ADSL_AFE, "adsl_afe", 80, NULL, 0, NULL, NULL),
+	PMU_GATE(DCDC_2V5, "dcdc_2v5", 81, NULL, CLK_IGNORE_UNUSED, NULL, NULL),
+	PMU_GATE(DCDC_1VX, "dcdc_1vx", 82, NULL, CLK_IGNORE_UNUSED, NULL, NULL),
+	PMU_GATE(DCDC_1V0, "dcdc_1v0", 83, NULL, CLK_IGNORE_UNUSED, NULL, NULL),
+};
+
+static void __init grx390_pmu_clk_gates_init_dt(struct device_node *np)
+{
+	ltq_xway_pmu_register_gates(np,
+				    grx390_pmu_gates,
+				    ARRAY_SIZE(grx390_pmu_gates),
+				    &xrx300_pmu_clk_gate_ops);
+}
+
+CLK_OF_DECLARE(pmu_grx390, "lantiq,grx390-pmu", grx390_pmu_clk_gates_init_dt);
---
+++ include/dt-bindings/clock/lantiq-xway-pmu.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2015 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Device Tree binding constants for the PMU module on lantiq xway SoCs.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_LANTIQ_XWAY_PMU_H
+#define _DT_BINDINGS_CLOCK_LANTIQ_XWAY_PMU_H
+
+#define PMU_GATE_DFEV0		0
+#define PMU_GATE_DFEV1		1
+#define PMU_GATE_PCI		2
+#define PMU_GATE_DMA		3
+#define PMU_GATE_USB0_CTRL	4
+#define PMU_GATE_USB1_CTRL	5
+#define PMU_GATE_USIF		6
+#define PMU_GATE_SPI		7
+#define PMU_GATE_DSL_DFE	8
+#define PMU_GATE_EBU		9
+#define PMU_GATE_STP		10
+#define PMU_GATE_GPTC		11
+#define PMU_GATE_AHBS		12
+#define PMU_GATE_FPI0		13
+#define PMU_GATE_WDT0		14
+#define PMU_GATE_WDT1		15
+#define PMU_GATE_AHBM		16
+#define PMU_GATE_SDIO		17
+#define PMU_GATE_UART0		18
+#define PMU_GATE_UART1		19
+#define PMU_GATE_PPE_QSB	20
+#define PMU_GATE_PPE_SLL01	21
+#define PMU_GATE_DEU		22
+#define PMU_GATE_PPE_TC		23
+#define PMU_GATE_PPE_EMA	24
+#define PMU_GATE_PPE_DPLUM	25
+#define PMU_GATE_PPE_DPLUS	26
+#define PMU_GATE_TDM		27
+#define PMU_GATE_SWITCH		28
+#define PMU_GATE_ETOP		29
+#define PMU_GATE_PPE_TOP	30
+#define PMU_GATE_EPHY		31
+#define PMU_GATE_GPHY		32
+#define PMU_GATE_VODEC		33
+#define PMU_GATE_VO_MIPS	34
+#define PMU_GATE_PCIE0_CTRL	35
+#define PMU_GATE_PCIE1_CTRL	36
+#define PMU_GATE_PCIE0_PDI	37
+#define PMU_GATE_PCIE0_MSI	38
+#define PMU_GATE_PCIE1_PDI	39
+#define PMU_GATE_PCIE1_MSI	40
+#define PMU_GATE_PCIE2_CTRL	41
+#define PMU_GATE_PCIE2_PDI	42
+#define PMU_GATE_PCIE2_MSI	43
+#define PMU_GATE_USB0_PHY	44
+#define PMU_GATE_USB1_PHY	45
+#define PMU_GATE_PCIE0_PHY	46
+#define PMU_GATE_PCIE1_PHY	47
+#define PMU_GATE_PCIE2_PHY	48
+#define PMU_GATE_PCIE0_CLK	49
+#define PMU_GATE_ADSL_AFE	50
+#define PMU_GATE_DCDC_2V5	51
+#define PMU_GATE_DCDC_1VX	52
+#define PMU_GATE_DCDC_1V0	53
+
+#define NUM_PMU_GATES		(PMU_GATE_DCDC_1V0 + 1)
+
+#endif /* _DT_BINDINGS_CLOCK_LANTIQ_XWAY_PMU_H */
