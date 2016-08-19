409befe7374a21795fe5e9453c21d454de04864b
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-08-20 01:10:16 +0200

    clk: gxbb: expose MPLL2 clock for use by DT
    This exposes the MPLL2 clock as this is one of the input clocks of the
    ethernet controller's internal mux.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Acked-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/dt-bindings/clock/gxbb-clkc.h
+++ include/dt-bindings/clock/gxbb-clkc.h
@@ -8,6 +8,7 @@
 #define CLKID_CPUCLK		1
 #define CLKID_FCLK_DIV2		4
 #define CLKID_CLK81		12
+#define CLKID_MPLL2		15
 #define CLKID_ETH		36
 #define CLKID_SD_EMMC_A		94
 #define CLKID_SD_EMMC_B		95
