a1666af7fdb48734784fa368d568fd4209c5b125
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-08-21 16:05:26 +0200

    usb experiments

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -28,6 +28,7 @@
 #define RTL8211F_INSR		0x1d
 #define RTL8211F_PAGE_SELECT	0x1f
 #define RTL8211F_TX_DELAY	0x100
+#define RTL8211F_LED_VAL	0x617f
 
 MODULE_DESCRIPTION("Realtek PHY driver");
 MODULE_AUTHOR("Johnson Leung");
@@ -93,24 +94,51 @@ static int rtl8211f_config_intr(struct phy_device *phydev)
 	return err;
 }
 
+static void rtl8211f_page_mask_bits(struct phy_device *phydev, u16 page,
+				    u16 reg, u16 mask, u16 value)
+{
+	u16 regval;
+
+	phy_write(phydev, RTL8211F_PAGE_SELECT, page);
+
+	regval = phy_read(phydev, reg);
+	regval &= ~mask;
+	regval |= (value & mask);
+
+	phy_write(phydev, reg, regval);
+
+	/* restore to default page 0 */
+	phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0);
+}
+
 static int rtl8211f_config_init(struct phy_device *phydev)
 {
 	int ret;
-	u16 reg;
 
 	ret = genphy_config_init(phydev);
 	if (ret < 0)
 		return ret;
 
-	if (phydev->interface == PHY_INTERFACE_MODE_RGMII) {
+	if (phydev->interface == PHY_INTERFACE_MODE_RGMII)
 		/* enable TXDLY */
-		phy_write(phydev, RTL8211F_PAGE_SELECT, 0xd08);
-		reg = phy_read(phydev, 0x11);
-		reg |= RTL8211F_TX_DELAY;
-		phy_write(phydev, 0x11, reg);
-		/* restore to default page 0 */
-		phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0);
-	}
+		rtl8211f_page_mask_bits(phydev, 0xd08, 0x11, RTL8211F_TX_DELAY,
+					RTL8211F_TX_DELAY);
+
+	/* Set green LED for Link, yellow LED for Active */
+	rtl8211f_page_mask_bits(phydev, 0xd04, 0x10, RTL8211F_LED_VAL,
+				RTL8211F_LED_VAL);
+
+#if 0
+	/* disable eee */
+	phy_write(phydev, MII_MMD_CTRL, 0x7);
+	phy_write(phydev, MII_MMD_DATA, 0x3c);
+	phy_write(phydev, MII_MMD_CTRL, 0x4007);
+	phy_write(phydev, MII_MMD_DATA, 0x0);
+
+	reg = phy_read(phydev, MII_CTRL1000);
+	reg &= ~ADVERTISE_1000HALF;
+	phy_write(phydev, MII_CTRL1000, reg);
+#endif
 
 	return 0;
 }
