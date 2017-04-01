5a9e4f6c02616c7febbc3de3916245dbba6c84c1
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-04-01 13:14:28 +0200

    m8 clk reset controller: WiP
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/reset/meson8b-clkc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Martin Blumenstingl <martin.blumenstingl@googlemail.com>.
+ *
+ * SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ */
+
+#ifndef DT_BINDINGS_CLKC_RESET_AMLOGIC_MESON8B_CLKC
+#define DT_BINDINGS_CLKC_RESET_AMLOGIC_MESON8B_CLKC
+
+#define CLKC_RESET_CPU3_SOFT_RESET			3
+#define CLKC_RESET_CPU2_SOFT_RESET			4
+#define CLKC_RESET_CPU1_SOFT_RESET			5
+#define CLKC_RESET_CPU0_SOFT_RESET			6
+
+#endif /* DT_BINDINGS_CLKC_RESET_AMLOGIC_MESON8B_CLKC */
