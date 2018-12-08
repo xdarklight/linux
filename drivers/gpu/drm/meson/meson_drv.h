e2d5f3004cb719f1ea05636e4542ce039643eb6b
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-12-08 12:29:32 +0100

    drm: meson: 32-bit Meson CVBS HACK
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- drivers/gpu/drm/meson/meson_drv.h
+++ drivers/gpu/drm/meson/meson_drv.h
@@ -19,10 +19,13 @@ struct meson_drm;
 struct meson_afbcd_ops;
 
 enum vpu_compatible {
-	VPU_COMPATIBLE_GXBB = 0,
-	VPU_COMPATIBLE_GXL  = 1,
-	VPU_COMPATIBLE_GXM  = 2,
-	VPU_COMPATIBLE_G12A = 3,
+	VPU_COMPATIBLE_M8 = 0,
+	VPU_COMPATIBLE_M8B = 1,
+	VPU_COMPATIBLE_M8M2 = 2,
+	VPU_COMPATIBLE_GXBB = 3,
+	VPU_COMPATIBLE_GXL  = 4,
+	VPU_COMPATIBLE_GXM  = 5,
+	VPU_COMPATIBLE_G12A = 6,
 };
 
 struct meson_drm_match_data {
@@ -145,6 +148,10 @@ struct meson_drm {
 		u64 modifier;
 		u32 format;
 	} afbcd;
+
+	struct {
+		uint32_t cntl1;
+	} cvbs;
 };
 
 static inline int meson_vpu_is_compatible(struct meson_drm *priv,
