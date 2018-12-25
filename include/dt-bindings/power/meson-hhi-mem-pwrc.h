d88bdafa5f4b8900b93d8853020650a89693fdb5
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-12-25 22:57:29 +0100

    dt-bindings: power: Meson HHI Memory power domains - WiP
    TODO:
    - dt-bindings documentation is missing
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/power/meson-hhi-mem-pwrc.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR MIT) */
+/*
+ * Amlogic Meson HHI Memory Power Domains
+ *
+ * Copyright (c) 2018 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ */
+
+#ifndef DT_BINDINGS_MESON_HHI_MEM_PWRC
+#define DT_BINDINGS_MESON_HHI_MEM_PWRC
+
+#define HHI_AUDIO_DSP_MEMORY_PD		0
+#define HHI_ETHERNET_MEMORY_PD		1
+#define HHI_HDMI_MEMORY_PD		2
+
+#endif /* DT_BINDINGS_MESON_HHI_MEM_PWRC */
