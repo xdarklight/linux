bd386a8f1fd9e1880d5e65e516e2a01297bb1c5c
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-12-25 22:56:48 +0100

    dt-bindings: power: Meson AO power domains - WiP
    TODO: dt-bindings description is missing (update pmu.txt ?)
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/power/meson-ao-pwrc.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR MIT) */
+/*
+ * Amlogic Meson Always-On Power Domains
+ *
+ * Copyright (c) 2018 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ */
+
+#ifndef DT_BINDINGS_MESON_AO_PWRC
+#define DT_BINDINGS_MESON_AO_PWRC
+
+#define AO_PWR_HCODEC_PD		0
+#define AO_PWR_VDEC_1_PD		1
+#define AO_PWR_VDEC_2_PD		2
+#define AO_PWR_VDEC_HVEC_PD		3
+#define AO_VPU_HDMI_ISO_PD		4
+#define AO_VPU_HDMI_PD			5
+#define AO_DEMOD_PD_COMB_PD		6
+#define AO_DEMOD_PD			7
+#define AO_WAVE420L_PD			8
+#define AO_ISO_HCODEC_PD		9
+#define AO_ISO_VDEC_1_PD		10
+#define AO_ISO_VDEC_2_PD		11
+#define AO_ISO_VDEC_HVEC_PD		12
+#define AO_ISO_WAVE420L_PD		13
+#define AO_ISO_DEMOD_PD			14
+
+#endif /* DT_BINDINGS_MESON_AO_PWRC */
