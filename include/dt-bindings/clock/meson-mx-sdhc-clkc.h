fda0b42e3c52c9f6ac1b5a46fb695041c315b692
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2019-04-13 18:46:08 +0200

    dt-bindings: mmc: Document the Amlogic Meson SDHC MMC host controller
    This documents the devicetree bindings for the SDHC MMC host controller
    found in Meson6, Meson8, Meson8b and Meson8m2 SoCs. It can use a
    bus-width of 1/4/8-bit and it supports eMMC spec 4.4x/4.5x including
    HS200 mode (up to 100MHz clock). It embeds an internal clock controller
    which outputs four clocks (mod_clk, sd_clk, tx_clk and rx_clk) and is
    fed by four external input clocks (clkin[0-3]). "pclk" is the module
    register clock, it has to be enabled to access the registers.
    
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/clock/meson-mx-sdhc-clkc.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#define SDHC_CLKID_SRC_SEL	0
+#define SDHC_CLKID_DIV		1
+#define SDHC_CLKID_MOD_CLK	2
+#define SDHC_CLKID_SD_CLK	3
+#define SDHC_CLKID_TX_CLK	4
+#define SDHC_CLKID_RX_CLK	5
