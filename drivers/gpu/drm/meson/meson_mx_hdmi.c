e3806a80457177c9a8b19780109a20ce0e518bd0
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2019-05-05 02:29:42 +0200

    drm/meson: add a driver for the HDMI controller on Meson8/Meson8b SoCs - WiP
    WiP
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ drivers/gpu/drm/meson/meson_mx_hdmi.c
@@ -0,0 +1,545 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ *
+ * All registers and magic values are taken from Amlogic's GPL kernel sources:
+ *   Copyright (C) 2010 Amlogic, Inc.
+ */
+
+#include <linux/bitfield.h>
+#include <linux/bits.h>
+#include <linux/clk.h>
+#include <linux/component.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/hdmi.h>
+#include <linux/irq.h>
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/phy/phy.h>
+#include <linux/regmap.h>
+
+#include <uapi/linux/media-bus-format.h>
+
+#include <drm/bridge/analogix_hdmi_14_tx.h>
+#include <drm/drm_atomic_helper.h>
+#include <drm/drm_bridge.h>
+#include <drm/drm_of.h>
+#include <drm/drm_print.h>
+#include <drm/drm_probe_helper.h>
+
+#include "meson_drv.h"
+#include "meson_registers.h"
+#include "meson_vclk.h"
+#include "meson_venc.h"
+
+#define HDMI_ADDR_PORT					0x0
+#define HDMI_DATA_PORT					0x4
+#define HDMI_CTRL_PORT					0x8
+	#define HDMI_CTRL_PORT_APB3_ERR_EN		BIT(15)
+
+#define HDMI_OTHER_CTRL0						0x8000
+#define HDMI_OTHER_CTRL1						0x8001
+	#define HDMI_OTHER_CTRL1_POWER_ON				BIT(15)
+
+#define HDMI_OTHER_STATUS0						0x8002
+#define HDMI_OTHER_CTRL2						0x8003
+#define HDMI_OTHER_INTR_MASKN						0x8004
+	#define HDMI_OTHER_INTR_MASKN_TX_EDID_INT_RISE			BIT(2)
+	#define HDMI_OTHER_INTR_MASKN_TX_HPD_INT_FALL			BIT(1)
+	#define HDMI_OTHER_INTR_MASKN_TX_HPD_INT_RISE			BIT(0)
+
+#define HDMI_OTHER_INTR_STAT						0x8005
+	#define HDMI_OTHER_INTR_STAT_EDID_RISING			BIT(2)
+	#define HDMI_OTHER_INTR_STAT_HPD_FALLING			BIT(1)
+	#define HDMI_OTHER_INTR_STAT_HPD_RISING				BIT(0)
+
+#define HDMI_OTHER_INTR_STAT_CLR					0x8006
+	#define HDMI_OTHER_INTR_STAT_CLR_EDID_RISING			BIT(2)
+	#define HDMI_OTHER_INTR_STAT_CLR_HPD_FALLING			BIT(1)
+	#define HDMI_OTHER_INTR_STAT_CLR_HPD_RISING			BIT(0)
+
+#define HDMI_OTHER_AVI_INTR_MASKN0					0x8008
+#define HDMI_OTHER_AVI_INTR_MASKN1					0x8009
+#define HDMI_OTHER_RX_AINFO_INTR_MASKN0					0x800a
+#define HDMI_OTHER_RX_AINFO_INTR_MASKN1					0x800b
+#define HDMI_OTHER_RX_PACKET_INTR_CLR					0x800c
+
+struct meson_mx_hdmi {
+	struct device			*dev;
+	struct drm_device		*drm;
+	struct anx_hdmi_14_tx		*anx;
+
+	struct clk			*pclk;
+	struct regmap			*other_regmap;
+
+	struct drm_encoder		encoder;
+	struct drm_bridge		bridge;
+
+	struct phy			*phy;
+	bool				phy_is_on;
+
+	unsigned int			output_bus_format;
+};
+
+#define to_meson_mx_hdmi(x) container_of(x, struct meson_mx_hdmi, x)
+
+static int meson_mx_hdmi_reg_read(void *context, unsigned int addr,
+				  unsigned int *data)
+{
+	void __iomem *base = context;
+
+	writel(addr, base + HDMI_ADDR_PORT);
+	writel(addr, base + HDMI_ADDR_PORT);
+
+	*data = readl(base + HDMI_DATA_PORT);
+
+	return 0;
+}
+
+static int meson_mx_hdmi_reg_write(void *context, unsigned int addr,
+				   unsigned int data)
+{
+	void __iomem *base = context;
+
+	writel(addr, base + HDMI_ADDR_PORT);
+	writel(addr, base + HDMI_ADDR_PORT);
+
+	writel(data, base + HDMI_DATA_PORT);
+
+	return 0;
+}
+
+static const struct regmap_config meson_mx_hdmi_bus_regmap_config = {
+	.name = "bus",
+	.reg_bits = 16,
+	.val_bits = 16,
+	.reg_stride = 1,
+	.reg_read = meson_mx_hdmi_reg_read,
+	.reg_write = meson_mx_hdmi_reg_write,
+	.fast_io = true,
+};
+
+static int meson_mx_hdmi_child_regmap_read(void *context, unsigned int addr,
+					   unsigned int *data)
+{
+	struct regmap *bus_regmap = context;
+
+	return regmap_read(bus_regmap, addr, data);
+}
+
+static int meson_mx_hdmi_child_regmap_write(void *context, unsigned int addr,
+					    unsigned int data)
+{
+	struct regmap *bus_regmap = context;
+
+	return regmap_write(bus_regmap, addr, data);
+}
+
+static const struct regmap_config meson_mx_hdmi_bridge_regmap_config = {
+	.name = "bridge",
+	.reg_bits = 16,
+	.val_bits = 8,
+	.reg_stride = 1,
+	.reg_read = meson_mx_hdmi_child_regmap_read,
+	.reg_write = meson_mx_hdmi_child_regmap_write,
+	.max_register = 0x7ff,
+	.fast_io = true,
+};
+
+static const struct regmap_range meson_mx_hdmi_other_regmap_ranges[] = {
+	regmap_reg_range(HDMI_OTHER_CTRL0, HDMI_OTHER_INTR_STAT_CLR),
+	regmap_reg_range(HDMI_OTHER_AVI_INTR_MASKN0,
+			 HDMI_OTHER_RX_PACKET_INTR_CLR),
+};
+
+static const struct regmap_access_table meson_mx_hdmi_other_regmap_access = {
+	.yes_ranges = meson_mx_hdmi_other_regmap_ranges,
+	.n_yes_ranges = ARRAY_SIZE(meson_mx_hdmi_other_regmap_ranges),
+};
+
+static const struct regmap_config meson_mx_hdmi_other_regmap_config = {
+	.name = "other",
+	.reg_bits = 16,
+	.val_bits = 16,
+	.reg_stride = 1,
+	.reg_read = meson_mx_hdmi_child_regmap_read,
+	.reg_write = meson_mx_hdmi_child_regmap_write,
+	.rd_table = &meson_mx_hdmi_other_regmap_access,
+	.wr_table = &meson_mx_hdmi_other_regmap_access,
+	.max_register = 0xffff,
+	.fast_io = true,
+};
+
+static int meson_mx_hdmi_bridge_atomic_check(struct drm_bridge *bridge,
+					struct drm_bridge_state *bridge_state,
+					struct drm_crtc_state *crtc_state,
+					struct drm_connector_state *conn_state)
+{
+	struct meson_mx_hdmi *hdmi = to_meson_mx_hdmi(bridge);
+
+	hdmi->output_bus_format = bridge_state->output_bus_cfg.format;
+
+	return 0;
+}
+
+static void meson_mx_hdmi_bridge_enable(struct drm_bridge *bridge)
+{
+	struct meson_mx_hdmi *hdmi = to_meson_mx_hdmi(bridge);
+	struct meson_drm *priv = hdmi->drm->dev_private;
+	int ret;
+
+	regmap_update_bits(hdmi->other_regmap, HDMI_OTHER_CTRL1,
+			   HDMI_OTHER_CTRL1_POWER_ON,
+			   HDMI_OTHER_CTRL1_POWER_ON);
+
+	meson_venc_hdmi_bridge_reset(priv);
+
+	if (!hdmi->phy_is_on) {
+		ret = phy_power_on(hdmi->phy);
+		if (ret)
+			drm_err(hdmi->drm, "Failed to turn on PHY\n");
+		else
+			hdmi->phy_is_on = true;
+	}
+
+	meson_venc_hdmi_encoder_enable(priv);
+}
+
+static void meson_mx_hdmi_bridge_disable(struct drm_bridge *bridge)
+{
+	struct meson_mx_hdmi *hdmi = to_meson_mx_hdmi(bridge);
+	struct meson_drm *priv = hdmi->drm->dev_private;
+	int ret;
+
+	meson_venc_hdmi_encoder_disable(priv);
+
+	regmap_update_bits(hdmi->other_regmap, HDMI_OTHER_CTRL1,
+			   HDMI_OTHER_CTRL1_POWER_ON, 0);
+
+	// TODO: assert resets?
+
+	if (hdmi->phy_is_on) {
+		ret = phy_power_off(hdmi->phy);
+		if (ret)
+			drm_err(hdmi->drm, "Failed to turn off PHY\n");
+		else
+			hdmi->phy_is_on = false;
+	}
+}
+
+static void meson_mx_hdmi_bridge_mode_set(struct drm_bridge *bridge,
+				const struct drm_display_mode *mode,
+				const struct drm_display_mode *adj_mode)
+{
+	unsigned int vclk_freq, venc_freq, hdmi_freq, phy_freq, ycrcb_map;
+	struct meson_mx_hdmi *hdmi = to_meson_mx_hdmi(bridge);
+	struct meson_drm *priv = hdmi->drm->dev_private;
+	int cea_mode = drm_match_cea_mode(mode);
+
+	switch (hdmi->output_bus_format) {
+	case MEDIA_BUS_FMT_YUV8_1X24:
+	case MEDIA_BUS_FMT_YUV10_1X30:
+	case MEDIA_BUS_FMT_YUV12_1X36:
+	case MEDIA_BUS_FMT_YUV16_1X48:
+	case MEDIA_BUS_FMT_UYVY8_1X16:
+	case MEDIA_BUS_FMT_UYVY10_1X20:
+	case MEDIA_BUS_FMT_UYVY12_1X24:
+		ycrcb_map = VPU_HDMI_OUTPUT_CRYCB;
+		break;
+
+	case MEDIA_BUS_FMT_RGB888_1X24:
+	case MEDIA_BUS_FMT_RGB101010_1X30:
+	case MEDIA_BUS_FMT_RGB121212_1X36:
+	case MEDIA_BUS_FMT_RGB161616_1X48:
+	default:
+		ycrcb_map = VPU_HDMI_OUTPUT_YCBCR;
+		break;
+
+	}
+
+	/* VENC + VENC-DVI Mode setup */
+	meson_venc_hdmi_mode_set(priv, cea_mode, ycrcb_map, false, mode);
+
+	vclk_freq = mode->clock;
+	phy_freq = vclk_freq * 10;
+
+	if (!cea_mode) {
+		meson_vclk_setup(priv, MESON_VCLK_TARGET_DMT, phy_freq,
+				 vclk_freq, vclk_freq, vclk_freq, false);
+	} else {
+		if (mode->flags & DRM_MODE_FLAG_DBLCLK)
+			vclk_freq *= 2;
+
+		venc_freq = vclk_freq;
+		hdmi_freq = vclk_freq;
+
+		if (meson_venc_hdmi_venc_repeat(cea_mode))
+			venc_freq *= 2;
+
+		vclk_freq = max(venc_freq, hdmi_freq);
+
+		if (mode->flags & DRM_MODE_FLAG_DBLCLK)
+			venc_freq /= 2;
+
+		drm_dbg(hdmi->drm, "vclk:%d venc=%d hdmi=%d enci=%d\n",
+			vclk_freq, venc_freq, hdmi_freq,
+			priv->venc.hdmi_use_enci);
+
+		meson_vclk_setup(priv, MESON_VCLK_TARGET_HDMI, phy_freq,
+				 vclk_freq, venc_freq, hdmi_freq,
+				 priv->venc.hdmi_use_enci);
+	}
+}
+
+static const struct drm_bridge_funcs meson_mx_hdmi_bridge_funcs = {
+	.atomic_duplicate_state	= drm_atomic_helper_bridge_duplicate_state,
+	.atomic_destroy_state	= drm_atomic_helper_bridge_destroy_state,
+	.atomic_reset		= drm_atomic_helper_bridge_reset,
+	.atomic_check		= meson_mx_hdmi_bridge_atomic_check,
+	.enable			= meson_mx_hdmi_bridge_enable,
+	.disable		= meson_mx_hdmi_bridge_disable,
+	.mode_set		= meson_mx_hdmi_bridge_mode_set,
+};
+
+static const struct drm_encoder_funcs meson_mx_hdmi_encoder_funcs = {
+	.destroy	= drm_encoder_cleanup,
+};
+
+static int meson_mx_hdmi_register(struct meson_mx_hdmi *hdmi)
+{
+	struct drm_bridge *next_bridge;
+	int ret;
+
+	hdmi->encoder.possible_crtcs = BIT(0);
+	ret = drm_encoder_init(hdmi->drm, &hdmi->encoder,
+			       &meson_mx_hdmi_encoder_funcs,
+			       DRM_MODE_ENCODER_TMDS, NULL);
+	if (ret)
+		return ret;
+
+	hdmi->bridge.funcs = &meson_mx_hdmi_bridge_funcs;
+	ret = drm_bridge_attach(&hdmi->encoder, &hdmi->bridge, NULL, 0);
+	if (ret)
+		return ret;
+
+	hdmi->anx = anx_hdmi_14_tx_probe(hdmi->dev);
+	if (IS_ERR(hdmi->anx))
+		return PTR_ERR(hdmi->anx);
+
+	next_bridge = of_drm_find_bridge(hdmi->dev->of_node);
+	if (next_bridge)
+		drm_bridge_attach(&hdmi->encoder, next_bridge,
+				  &hdmi->bridge, 0);
+
+	return 0;
+}
+
+static irqreturn_t meson_mx_hdmi_irq_thread(int irq, void *dev_id)
+{
+	struct meson_mx_hdmi *hdmi = dev_id;
+
+	drm_helper_hpd_irq_event(hdmi->encoder.dev);
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t meson_mx_hdmi_irq_handler(int irq, void *dev_id)
+{
+	struct meson_mx_hdmi *hdmi = dev_id;
+	irqreturn_t ret = IRQ_WAKE_THREAD;
+	u32 regval;
+
+	regmap_read(hdmi->other_regmap, HDMI_OTHER_INTR_STAT, &regval);
+	if (!regval)
+		return IRQ_NONE;
+
+	if (!(regval & (HDMI_OTHER_INTR_STAT_EDID_RISING |
+			HDMI_OTHER_INTR_STAT_HPD_FALLING |
+			HDMI_OTHER_INTR_STAT_HPD_RISING))) {
+		drm_warn(hdmi->drm, "IRQ status has unknown bit set: 0x%04x\n",
+			 regval);
+		ret = IRQ_HANDLED;
+	}
+
+	regmap_write(hdmi->other_regmap, HDMI_OTHER_INTR_STAT_CLR, regval);
+
+	return ret;
+}
+
+static void meson_mx_hdmi_exit(struct meson_mx_hdmi *hdmi)
+{
+	if (hdmi->anx)
+		anx_hdmi_14_tx_remove(hdmi->anx);
+
+	phy_exit(hdmi->phy);
+
+	clk_disable_unprepare(hdmi->pclk);
+}
+
+static int meson_mx_hdmi_init(struct meson_mx_hdmi *hdmi, void __iomem *base)
+{
+	u32 regval;
+	int ret;
+
+	ret = clk_prepare_enable(hdmi->pclk);
+	if (ret) {
+		drm_err(hdmi->drm, "Failed to enable the pclk: %d\n", ret);
+		return ret;
+	}
+
+	regval = readl(base + HDMI_CTRL_PORT);
+	regval |= HDMI_CTRL_PORT_APB3_ERR_EN;
+	writel(regval, base + HDMI_CTRL_PORT);
+
+	ret = phy_init(hdmi->phy);
+	if (ret) {
+		drm_err(hdmi->drm, "Failed to initialize the PHY: %d\n", ret);
+		return ret;
+	}
+
+	/* clear any pending interrupt */
+	regmap_write(hdmi->other_regmap, HDMI_OTHER_INTR_STAT_CLR,
+		     HDMI_OTHER_INTR_STAT_CLR_EDID_RISING |
+		     HDMI_OTHER_INTR_STAT_CLR_HPD_FALLING |
+		     HDMI_OTHER_INTR_STAT_CLR_HPD_RISING);
+
+	/* unmask (= enable) all interrupts */
+	regmap_write(hdmi->other_regmap, HDMI_OTHER_INTR_MASKN,
+		     HDMI_OTHER_INTR_MASKN_TX_EDID_INT_RISE |
+		     HDMI_OTHER_INTR_MASKN_TX_HPD_INT_FALL |
+		     HDMI_OTHER_INTR_MASKN_TX_HPD_INT_RISE);
+
+	return 0;
+}
+
+static int meson_mx_hdmi_bind(struct device *dev, struct device *master,
+			      void *data)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct regmap *bus_regmap, *bridge_regmap;
+	struct drm_device *drm = data;
+	struct meson_mx_hdmi *hdmi;
+	struct resource *res;
+	void __iomem *base;
+	int irq, ret;
+
+	hdmi = devm_kzalloc(dev, sizeof(*hdmi), GFP_KERNEL);
+	if (!hdmi)
+		return -ENOMEM;
+
+	hdmi->dev = dev;
+	hdmi->drm = drm;
+
+	dev_set_drvdata(dev, hdmi);
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(base))
+		return PTR_ERR(base);
+
+	bus_regmap = devm_regmap_init(dev, NULL, base,
+				      &meson_mx_hdmi_bus_regmap_config);
+	if (IS_ERR(bus_regmap))
+		return PTR_ERR(bus_regmap);
+
+	bridge_regmap = devm_regmap_init(dev, NULL, bus_regmap,
+					 &meson_mx_hdmi_bridge_regmap_config);
+	if (IS_ERR(bridge_regmap))
+		return PTR_ERR(bridge_regmap);
+
+	hdmi->other_regmap = devm_regmap_init(dev, NULL, bus_regmap,
+					&meson_mx_hdmi_other_regmap_config);
+	if (IS_ERR(hdmi->other_regmap))
+		return PTR_ERR(hdmi->other_regmap);
+
+	irq = platform_get_irq(pdev, 0);
+	if (irq < 0)
+		return irq;
+
+	hdmi->pclk = devm_clk_get(hdmi->dev, "pclk");
+	if (IS_ERR(hdmi->pclk)) {
+		ret = PTR_ERR(hdmi->pclk);
+		drm_err(drm, "Failed to get the pclk: %d\n", ret);
+		return ret;
+	}
+
+	hdmi->phy = devm_phy_get(hdmi->dev, "hdmi");
+	if (IS_ERR(hdmi->phy)) {
+		ret = PTR_ERR(hdmi->phy);
+		drm_err(drm, "Failed to get the PHY: %d\n", ret);
+		return ret;
+	}
+
+	ret = meson_mx_hdmi_init(hdmi, base);
+	if (ret)
+		return ret;
+
+	ret = meson_mx_hdmi_register(hdmi);
+	if (ret) {
+		drm_err(drm, "Failed to register HDMI: %d\n", ret);
+		goto err_hdmi_exit;
+	}
+
+	ret = devm_request_threaded_irq(dev, irq, meson_mx_hdmi_irq_handler,
+					meson_mx_hdmi_irq_thread, 0, NULL,
+					hdmi);
+	if (ret) {
+		drm_err(drm, "Failed to request threaded irq: %d\n", ret);
+		goto err_hdmi_exit;
+	}
+
+	return 0;
+
+err_hdmi_exit:
+	meson_mx_hdmi_exit(hdmi);
+	return ret;
+}
+
+static void meson_mx_hdmi_unbind(struct device *dev, struct device *master,
+				 void *data)
+{
+	struct meson_mx_hdmi *hdmi = dev_get_drvdata(dev);
+
+	hdmi->encoder.funcs->destroy(&hdmi->encoder);
+
+	meson_mx_hdmi_exit(hdmi);
+}
+
+static const struct component_ops meson_mx_hdmi_component_ops = {
+	.bind = meson_mx_hdmi_bind,
+	.unbind = meson_mx_hdmi_unbind,
+};
+
+static int meson_mx_hdmi_probe(struct platform_device *pdev)
+{
+	return component_add(&pdev->dev, &meson_mx_hdmi_component_ops);
+}
+
+static int meson_mx_hdmi_remove(struct platform_device *pdev)
+{
+	component_del(&pdev->dev, &meson_mx_hdmi_component_ops);
+
+	return 0;
+}
+
+static const struct of_device_id meson_mx_hdmi_of_table[] = {
+	{ .compatible = "amlogic,meson8-hdmi-tx" },
+	{ .compatible = "amlogic,meson8b-hdmi-tx" },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, meson_mx_hdmi_of_table);
+
+static struct platform_driver meson_mx_hdmi_platform_driver = {
+	.probe		= meson_mx_hdmi_probe,
+	.remove		= meson_mx_hdmi_remove,
+	.driver		= {
+		.name		= "meson-mx-hdmi",
+		.of_match_table	= meson_mx_hdmi_of_table,
+	},
+};
+module_platform_driver(meson_mx_hdmi_platform_driver);
+
+MODULE_AUTHOR("Martin Blumenstingl <martin.blumenstingl@googlemail.com>");
+MODULE_DESCRIPTION("Amlogic Meson8 and Meson8b HDMI TX DRM driver");
+MODULE_LICENSE("GPL v2");
