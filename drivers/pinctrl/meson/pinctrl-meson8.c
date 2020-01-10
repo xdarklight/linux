2fd7a6fea9cbf6d81034f2a097c37918447558e1
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-01-10 01:48:59 +0100

    pinctrl: meson: meson8: fix the supported bits of GPIO_TEST_N
    GPIO_TEST_N is special because it is not part of the documentation in
    the public S805 datasheet. There is no documented way to switch it from
    an output to an input. The vendor kernel uses the output register to
    read the pin value back. The GPIO interrupt controller does not seem to
    have a bit for this pad.
    
    The vendor kernel uses AO_GPIO_O_EN_N[31] for the GPIO output value
    (setting the bit means HIGH, clearing it means LOW).
    The bias can be configured using AO_RTI_PULL_UP_REG[14] (to set the bias
    to pull-up or pull-down) and AO_RTI_PULL_UP_REG[30] (to enable the
    pull-up/down).
    Set the irq, dir and in registers and bits to -1 to mark them as
    unsupported and use the pullen, pull, out and in registers and bits from
    the vendor kernel.
    
    Additionally the vendor kernel sets bit AO_SECURE_REG0[0] (0xda004000)
    with the comment "out put enable" (it does the same for the other
    special GPIO: GPIO_BSD_EN). This bit is set on all of my boards (it may
    be set by the boot ROM or the vendor u-boot). Support for managing this
    extra bit will be added in the future.
    
    Fixes: 6ac730951104a4 ("pinctrl: add driver for Amlogic Meson SoCs")
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- drivers/pinctrl/meson/pinctrl-meson8.c
+++ drivers/pinctrl/meson/pinctrl-meson8.c
@@ -1075,8 +1075,14 @@ static struct meson_bank meson8_cbus_banks[] = {
 };
 
 static struct meson_bank meson8_aobus_banks[] = {
-	/*   name    first     last         irq    pullen  pull    dir     out     in  */
-	BANK("AO",   GPIOAO_0, GPIO_TEST_N, 0, 13, 0, 16,  0,  0,  0,  0,  0, 16,  1,  0),
+	/*
+	 *   name      first        last          irq      pullen   pull
+	 *   dir      out     in
+	 */
+	BANK("AO",     GPIOAO_0,    GPIO_BSD_EN,   0, 13,  0, 16,  0,  0,
+	      0,  0,  0, 16,  1,  0),
+	BANK("TEST_N", GPIO_TEST_N, GPIO_TEST_N,  -1, -1,  0, 30,  0, 14,
+	     -1, -1,  0, 31,  0, 31),
 };
 
 static struct meson_pinctrl_data meson8_cbus_pinctrl_data = {
