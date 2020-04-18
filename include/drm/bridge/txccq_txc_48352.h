c5f938f13fd109340baa6d8001208cd9a08f062b
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-04-18 22:38:18 +0200

    drm/bridge: transwitch: Add a driver for the TXC-48352 HDMI 1.4 TX IP
    WiP
    
    This driver is based on the Amlogic implementation from their 3.10
    vendor kernel.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/drm/bridge/txccq_txc_48352.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2020 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ */
+
+#ifndef __DRM_BRIDGE_TXCCQ_TXC_48352__
+#define __DRM_BRIDGE_TXCCQ_TXC_48352__
+
+struct txc_48352;
+
+struct txc_48352 *txc_48352_probe(struct device *dev);
+void txc_48352_remove(struct txc_48352 *priv);
+
+#endif /* __DRM_BRIDGE_TXCCQ_TXC_48352__ */
