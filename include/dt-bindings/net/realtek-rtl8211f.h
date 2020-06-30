b6d155f4eca129579709a865ce2b89de3173812d
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-06-30 23:24:16 +0200

    dt-bindings: net: Add documentation for the RTL8211F Ethernet PHY
    The Realtek RTL8211F Ethernet PHY connects to the MAC using RGMII. It
    can drive up to three LEDs with the following status/functionality:
    - 10Mbit/s link
    - 100Mbit/s link
    - 1000Mbit/s link
    - RX/TX activity
    - EEE enabled
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ include/dt-bindings/net/realtek-rtl8211f.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_REALTEK_RTL8211F_H
+#define _DT_BINDINGS_REALTEK_RTL8211F_H
+
+#define RTL8211F_LED_LINK_10			(1 << 0)
+#define RTL8211F_LED_LINK_100			(1 << 1)
+#define RTL8211F_LED_LINK_1000			(1 << 2)
+#define RTL8211F_LED_ACTIVITY			(1 << 3)
+#define RTL8211F_LED_EEE			(1 << 4)
+
+#endif /* _DT_BINDINGS_REALTEK_RTL8211F_H */
