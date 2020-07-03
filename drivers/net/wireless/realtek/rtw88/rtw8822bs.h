f64fe6dda3361e2bee6565679725df0ff45ca948
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-07-04 00:56:34 +0200

    rtw88: sdio: Initial SDIO support
    It kinda works.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8822bs.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2021 Jernej Skrabec <jernej.skrabec@gmail.com>
+ */
+
+#ifndef __RTW_8822BS_H_
+#define __RTW_8822BS_H_
+
+extern struct rtw_chip_info rtw8822b_hw_spec;
+
+#endif
