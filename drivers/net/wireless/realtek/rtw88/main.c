dd44e08220266eea579bd08c6bb54c92943920fd
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-07-04 00:56:34 +0200

    rtw88: sdio: HACK
    HACK
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- drivers/net/wireless/realtek/rtw88/main.c
+++ drivers/net/wireless/realtek/rtw88/main.c
@@ -1166,6 +1166,10 @@ static int rtw_chip_parameter_setup(struct rtw_dev *rtwdev)
 		rtwdev->hci.rpwm_addr = 0x03d9;
 		rtwdev->hci.cpwm_addr = 0x03da;
 		break;
+	case RTW_HCI_TYPE_SDIO:
+		rtwdev->hci.rpwm_addr = REG_SDIO_HRPWM1;
+		rtwdev->hci.cpwm_addr = REG_SDIO_HCPWM1_V2;
+		break;
 	default:
 		rtw_err(rtwdev, "unsupported hci type\n");
 		return -EINVAL;
