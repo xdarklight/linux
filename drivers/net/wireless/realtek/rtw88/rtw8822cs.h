f512b1f14b59744dbddea5e378228300e7ee3a1b
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-07-04 00:56:34 +0200

    rtw88: Add support for the SDIO based RTL8822CS chipset
    Wire up RTL8822CS chipset support using the new rtw88 SDIO HCI code as
    well as the existing RTL8822C chipset code.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8822cs.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2021 Martin Blumenstingl
+ */
+
+#ifndef __RTW_8822CS_H_
+#define __RTW_8822CS_H_
+
+extern struct rtw_chip_info rtw8822c_hw_spec;
+
+#endif
