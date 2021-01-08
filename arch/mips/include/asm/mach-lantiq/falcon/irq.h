48fd27aac0487e33781f4d30550874bfd9e897df
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2021-01-08 23:49:40 +0100

    MIPS: lantiq: remove the custom interrupt controller implementations
    Remove the logic for the ICU and EIU interrupt-controller from
    arch/mips/lantiq/irq.c now that they have their own drivers.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- arch/mips/include/asm/mach-lantiq/falcon/irq.h
+++ arch/mips/include/asm/mach-lantiq/falcon/irq.h
@@ -7,8 +7,6 @@
 #ifndef __FALCON_IRQ_H
 #define __FALCON_IRQ_H
 
-#include <falcon_irq.h>
-
 #define NR_IRQS 328
 
 #include <asm/mach-generic/irq.h>
--- arch/mips/include/asm/mach-lantiq/xway/irq.h
+++ arch/mips/include/asm/mach-lantiq/xway/irq.h
@@ -7,8 +7,6 @@
 #ifndef __LANTIQ_IRQ_H
 #define __LANTIQ_IRQ_H
 
-#include <lantiq_irq.h>
-
 #define NR_IRQS 256
 
 #include <asm/mach-generic/irq.h>
