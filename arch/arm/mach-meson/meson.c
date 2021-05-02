17d15e63d9f378f503a0a6b2b0362f1627fc4a4e
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2021-05-03 00:34:53 +0200

    ARM: meson: Add support for the TrustZone firmware
    Amlogic Meson6/8/8b/8m2 SoCs can optionally use a TrustZone secure
    firmware. This prevents anything outside of the TEE (Trusted
    Execution Environment aka TrustZone secure firmware) from accessing
    certain functionality of these SoCs, such as (but not limited to):
    Bringing up/down secondary SMP cores, accessing the eFuse and getting
    the SoC misc version.
    ARM SMCCC is used for communication with the TrustZone secure
    firmware.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

--- arch/arm/mach-meson/meson.c
+++ arch/arm/mach-meson/meson.c
@@ -5,6 +5,8 @@
 
 #include <asm/mach/arch.h>
 
+#include "tz_firmware.h"
+
 static const char * const meson_common_board_compat[] = {
 	"amlogic,meson6",
 	"amlogic,meson8",
@@ -17,4 +19,6 @@ DT_MACHINE_START(MESON, "Amlogic Meson platform")
 	.dt_compat	= meson_common_board_compat,
 	.l2c_aux_val	= 0,
 	.l2c_aux_mask	= ~0,
+	.init_early	= meson_mx_trustzone_firmware_init,
+	.reserve	= meson_mx_trustzone_firmware_reserve_mem,
 MACHINE_END
