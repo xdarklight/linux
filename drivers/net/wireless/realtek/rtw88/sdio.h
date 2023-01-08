729ef9a58c5052561a947b99930ac306f9706bd4
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2023-01-08 22:46:00 +0100

    Revert "wifi: rtw88: SDIO ieee80211_{stop,wake}_queue() experiments - FIXME"
    This reverts commit 44853fe43847a3c775642d3e664cba5fedec039e.

--- drivers/net/wireless/realtek/rtw88/sdio.h
+++ drivers/net/wireless/realtek/rtw88/sdio.h
@@ -144,12 +144,6 @@ struct rtw_sdio_work_data {
 	struct rtw_dev *rtwdev;
 };
 
-struct rtw_sdio_tx_queue {
-	struct sk_buff_head head;
-	/* protect against concurrent ieee80211_{stop,wake}_queue() calls */
-	spinlock_t lock;
-};
-
 struct rtw_sdio {
 	struct sdio_func *sdio_func;
 
@@ -162,15 +156,7 @@ struct rtw_sdio {
 
 	struct workqueue_struct *txwq;
 	struct rtw_sdio_work_data *tx_handler_data;
-
-	struct rtw_sdio_tx_queue tx_queue[RTK_MAX_TX_QUEUE_NUM];
-
-	u16 tx_pages_free_high;
-	u16 tx_pages_free_normal;
-	u16 tx_pages_free_low;
-	u16 tx_pages_free_public;
-	u16 tx_pages_free_extra;
-	bool free_tx_pages_initialized;
+	struct sk_buff_head tx_queue[RTK_MAX_TX_QUEUE_NUM];
 };
 
 extern const struct dev_pm_ops rtw_sdio_pm_ops;
