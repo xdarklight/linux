fb77c7003e01c8459b8a99c0c7a194f9fb33f3e1
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2023-03-21 11:37:23 +0100

    wifi: rtw88: BROKEN - RTL8723BS support - NOT WORKING!
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8723b_table.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2018-2019  Realtek Corporation
+ * Copyright(c) 2022 Martin Blumenstingl <martin.blumenstingl@googlemail.com>
+ */
+
+#ifndef __RTW8723B_TABLE_H__
+#define __RTW8723B_TABLE_H__
+
+extern const struct rtw_table rtw8723b_mac_tbl;
+extern const struct rtw_table rtw8723b_agc_tbl;
+extern const struct rtw_table rtw8723b_bb_tbl;
+extern const struct rtw_table rtw8723b_bb_pg_tbl;
+extern const struct rtw_table rtw8723b_rf_a_tbl;
+extern const struct rtw_table rtw8723b_txpwr_lmt_tbl;
+
+#endif
